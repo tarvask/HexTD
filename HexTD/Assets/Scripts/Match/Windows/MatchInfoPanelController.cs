2ab2559e47d5fd6e83b817cb78e4123de0bc52eb
Vasily Tarasevich <taravask@gmail.com>
2023-04-06 16:15:57 +0300

    Fixed missing sprites and removed unneeded logic from some windows

--- HexTD/Assets/Scripts/Match/Windows/MatchInfoPanelController.cs
+++ HexTD/Assets/Scripts/Match/Windows/MatchInfoPanelController.cs
@@ -16,26 +16,23 @@ namespace Match.Windows
             public MatchInfoPanelView View { get; }
             public Camera MainCamera { get; }
             public Canvas Canvas { get; }
-            public MobConfigRetriever MobConfigRetriever { get; }
             public WaveParams[] Waves { get; }
             
             public ReactiveCommand<HealthInfo> EnemyCastleHealthChangedReactiveCommand { get; }
             public ReactiveCommand<HealthInfo> OurCastleHealthChangedReactiveCommand { get; }
             public ReactiveCommand<float> WaveStartedReactiveCommand { get; }
             public ReactiveCommand<float> BetweenWavesPlanningStartedReactiveCommand { get; }
-            public ReactiveCommand<float> ArtifactChoosingStartedReactiveCommand { get; }
             public ReactiveCommand<int> WaveNumberChangedReactiveCommand { get; }
             public ReactiveCommand<int> OurGoldenCoinsCountChangedReactiveCommand { get; }
             public ReactiveCommand<int> OurGoldenCoinsIncomeChangedReactiveCommand { get; }
             public ReactiveCommand<int> OurCrystalsCountChangedReactiveCommand { get; }
 
-            public Context(MatchInfoPanelView view, Camera mainCamera, Canvas canvas, MobConfigRetriever mobConfigRetriever, WaveParams[] waves,
+            public Context(MatchInfoPanelView view, Camera mainCamera, Canvas canvas, WaveParams[] waves,
                 
                 ReactiveCommand<HealthInfo> enemyCastleHealthChangedReactiveCommand,
                 ReactiveCommand<HealthInfo> ourCastleHealthChangedReactiveCommand,
                 ReactiveCommand<float> waveStartedReactiveCommand,
                 ReactiveCommand<float> betweenWavesPlanningStartedReactiveCommand,
-                ReactiveCommand<float> artifactChoosingStartedReactiveCommand,
                 ReactiveCommand<int> waveNumberChangedReactiveCommand,
                 ReactiveCommand<int> ourGoldenCoinsCountChangedReactiveCommand,
                 ReactiveCommand<int> ourGoldenCoinsIncomeChangedReactiveCommand,
@@ -44,14 +41,12 @@ namespace Match.Windows
                 View = view;
                 MainCamera = mainCamera;
                 Canvas = canvas;
-                MobConfigRetriever = mobConfigRetriever;
                 Waves = waves;
 
                 EnemyCastleHealthChangedReactiveCommand = enemyCastleHealthChangedReactiveCommand;
                 OurCastleHealthChangedReactiveCommand = ourCastleHealthChangedReactiveCommand;
                 WaveStartedReactiveCommand = waveStartedReactiveCommand;
                 BetweenWavesPlanningStartedReactiveCommand = betweenWavesPlanningStartedReactiveCommand;
-                ArtifactChoosingStartedReactiveCommand = artifactChoosingStartedReactiveCommand;
                 WaveNumberChangedReactiveCommand = waveNumberChangedReactiveCommand;
                 OurGoldenCoinsCountChangedReactiveCommand = ourGoldenCoinsCountChangedReactiveCommand;
                 OurGoldenCoinsIncomeChangedReactiveCommand = ourGoldenCoinsIncomeChangedReactiveCommand;
@@ -61,7 +56,6 @@ namespace Match.Windows
 
         private readonly Context _context;
         private readonly MatchInfoRoundStartInfoPanelController _roundStartInfoPanelController;
-        private readonly Dictionary<byte, byte> _enemyReinforcementsItems;
         private int _waveNumber;
         private float _currentTimer;
         private int _currentTimerInt;
@@ -78,46 +72,15 @@ namespace Match.Windows
             MatchInfoRoundStartInfoPanelController.Context roundStartInfoPanelControllerContext =
                 new MatchInfoRoundStartInfoPanelController.Context(_context.View.RoundStartInfoPanel);
             _roundStartInfoPanelController = AddDisposable(new MatchInfoRoundStartInfoPanelController(roundStartInfoPanelControllerContext));
-            _enemyReinforcementsItems = new Dictionary<byte, byte>();
-            
+
             _context.EnemyCastleHealthChangedReactiveCommand.Subscribe(OnEnemyCastleHealthChangedEventHandler);
             _context.OurCastleHealthChangedReactiveCommand.Subscribe(OnOurCastleHealthChangedEventHandler);
             _context.WaveNumberChangedReactiveCommand.Subscribe(OnWaveNumberChangedEventHandler);
-            _context.ArtifactChoosingStartedReactiveCommand.Subscribe(OnArtifactChoosingStartedEventHandler);
             _context.BetweenWavesPlanningStartedReactiveCommand.Subscribe(OnBetweenWavesPlanningStartedEventHandler);
             _context.WaveStartedReactiveCommand.Subscribe(OnWaveStartedEventHandler);
             _context.OurGoldenCoinsCountChangedReactiveCommand.Subscribe(OnOurGoldenCoinsCountChangedEventHandler);
             _context.OurGoldenCoinsIncomeChangedReactiveCommand.Subscribe(OnOurGoldenCoinsIncomeChangedEventHandler);
             _context.OurCrystalsCountChangedReactiveCommand.Subscribe(OnOurCrystalsCountChangedEventHandler);
-
-            // show contents of Wave 1
-            RefreshReinforcementsInfo(0);
-        }
-
-        public void AddReinforcementsItem(byte mobId)
-        {
-            if (_enemyReinforcementsItems.ContainsKey(mobId))
-                _enemyReinforcementsItems[mobId]++;
-            else
-            {
-                _enemyReinforcementsItems.Add(mobId, 1);
-                _context.View.ReinforcementsItems[_enemyReinforcementsItems.Count - 1].sprite =
-                    _context.MobConfigRetriever.GetMobById(mobId).Icon;
-            }
-        }
-        
-        public void RemoveReinforcementsItem(byte mobId)
-        {
-            if (_enemyReinforcementsItems.ContainsKey(mobId))
-            {
-                _enemyReinforcementsItems[mobId]--;
-
-                if (_enemyReinforcementsItems[mobId] == 0)
-                {
-                    _enemyReinforcementsItems.Remove(mobId);
-                    _context.View.ReinforcementsItems[_enemyReinforcementsItems.Count - 1].sprite = null;
-                }
-            }
         }
 
         private void OnEnemyCastleHealthChangedEventHandler(HealthInfo castleHealthInfo)
@@ -145,13 +108,6 @@ namespace Match.Windows
             _context.View.NextRoundStartWithTimer.gameObject.SetActive(true);
         }
 
-        private void OnArtifactChoosingStartedEventHandler(float artifactChoosingDuration)
-        {
-            _currentTimer = artifactChoosingDuration + _context.Waves[Mathf.Min(_waveNumber - 1, _context.Waves.Length - 1)].PauseBeforeWave;
-            _context.View.CurrentRoundWithTimer.gameObject.SetActive(false);
-            _context.View.NextRoundStartWithTimer.gameObject.SetActive(true);
-        }
-
         private void OnWaveStartedEventHandler(float waveInfoDuration)
         {
             _waveSpawningInProcess = true;
@@ -159,9 +115,7 @@ namespace Match.Windows
             _context.View.CurrentRoundWithTimer.gameObject.SetActive(true);
             _context.View.NextRoundStartWithTimer.gameObject.SetActive(false);
             
-            _roundStartInfoPanelController.Show(_waveNumber, _context.View.WaveElements, _context.View.ReinforcementsItems,
-                waveInfoDuration);
-            RefreshReinforcementsInfo(_waveNumber);
+            _roundStartInfoPanelController.Show(_waveNumber, waveInfoDuration);
         }
 
         private void OnOurGoldenCoinsCountChangedEventHandler(int newGoldenCoinsCount)
@@ -177,25 +131,6 @@ namespace Match.Windows
         private void OnOurCrystalsCountChangedEventHandler(int newCrystalsCount)
         {
             _context.View.OurCrystalsCountText.text = $"{newCrystalsCount}";
-        }
-
-        private void RefreshReinforcementsInfo(int waveNumber)
-        {
-            int nextWaveIndex = Mathf.Clamp(waveNumber, 0, _context.Waves.Length - 1);
-            // drop reinforcements for next round
-            _enemyReinforcementsItems.Clear();
-
-            for (int waveElementIndex = 0; waveElementIndex < _context.View.WaveElements.Length; waveElementIndex++)
-            {
-                if (waveElementIndex < _context.Waves[nextWaveIndex].Elements.Length)
-                {
-                    _context.View.WaveElements[waveElementIndex].sprite =
-                        _context.MobConfigRetriever
-                            .GetMobById(_context.Waves[nextWaveIndex].Elements[waveElementIndex].MobId).Icon;
-                }
-                else
-                    _context.View.WaveElements[waveElementIndex].sprite = null;
-            }
         }
 
         public void OuterLogicUpdate(float frameLength)
