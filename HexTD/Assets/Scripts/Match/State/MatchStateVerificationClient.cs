d3fca697229c8a05516a01689c5a387861a9f760
Vasily Tarasevich <taravask@gmail.com>
2023-07-09 23:41:16 +0300

    Added logic of decoding checksums' comparison to text

--- HexTD/Assets/Scripts/Match/State/MatchStateVerificationClient.cs
+++ HexTD/Assets/Scripts/Match/State/MatchStateVerificationClient.cs
@@ -71,7 +71,53 @@ namespace Match.State
         {
             _context.RequestMatchStateReactiveCommand.Execute();
         }
+
+        // only valid for objects' count tactic
+        private string GetDecodedMessageForCheckSumComparison(
+            MatchStateCheckSum clientCheckSum,
+            MatchStateCheckSum serverCheckSum)
+        {
+            var decodedMessage = new StringBuilder(256);
+
+            // player1
+            int clientPlayer1ProjectilesCount = clientCheckSum.Player1CheckSum % 256;
+            int serverPlayer1ProjectilesCount = serverCheckSum.Player1CheckSum % 256;
+
+            if (clientPlayer1ProjectilesCount != serverPlayer1ProjectilesCount)
+                decodedMessage.Append($"Player1 projectiles: client={clientPlayer1ProjectilesCount}, server={serverPlayer1ProjectilesCount}\n");
             
+            int clientPlayer1MobsCount = clientCheckSum.Player1CheckSum / 256 % 256;
+            int serverPlayer1MobsCount = serverCheckSum.Player1CheckSum / 256 % 256;
+            
+            if (clientPlayer1MobsCount != serverPlayer1MobsCount)
+                decodedMessage.Append($"Player1 mobs: client={clientPlayer1MobsCount}, server={serverPlayer1MobsCount}\n");
+            
+            int clientPlayer1TowersCount = clientCheckSum.Player1CheckSum / 256 / 256 % 256;
+            int serverPlayer1TowersCount = serverCheckSum.Player1CheckSum / 256 / 256 % 256;
+            
+            if (clientPlayer1TowersCount != serverPlayer1TowersCount)
+                decodedMessage.Append($"Player1 towers: client={clientPlayer1TowersCount}, server={serverPlayer1TowersCount}\n");
+            
+            // player2
+            int clientPlayer2ProjectilesCount = clientCheckSum.Player2CheckSum % 256;
+            int serverPlayer2ProjectilesCount = serverCheckSum.Player2CheckSum % 256;
+
+            if (clientPlayer2ProjectilesCount != serverPlayer2ProjectilesCount)
+                decodedMessage.Append($"Player2 projectiles: client={clientPlayer2ProjectilesCount}, server={serverPlayer2ProjectilesCount}\n");
+            
+            int clientPlayer2MobsCount = clientCheckSum.Player2CheckSum / 256 % 256;
+            int serverPlayer2MobsCount = serverCheckSum.Player2CheckSum / 256 % 256;
+            
+            if (clientPlayer2MobsCount != serverPlayer2MobsCount)
+                decodedMessage.Append($"Player2 mobs: client={clientPlayer2MobsCount}, server={serverPlayer2MobsCount}\n");
+            
+            int clientPlayer2TowersCount = clientCheckSum.Player2CheckSum / 256 / 256 % 256;
+            int serverPlayer2TowersCount = serverCheckSum.Player2CheckSum / 256 / 256 % 256;
+            
+            if (clientPlayer2TowersCount != serverPlayer2TowersCount)
+                decodedMessage.Append($"Player2 towers: client={clientPlayer2TowersCount}, server={serverPlayer2TowersCount}\n");
+
+            return decodedMessage.ToString();
         }
     }
 }
